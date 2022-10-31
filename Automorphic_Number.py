dc900b587f1ef19a3b47e2cc4f4a94304ea38006
21A91A04P6 <98083918+21A91A04P6@users.noreply.github.com>
2022-10-31 15:02:45 +0530

    n=int(input()) temp=n s=0 while(n!=0):     n//=10     s=s+1 q=temp**2 p=q%(10**s) if(p==temp):     print("Automorphic Number") else:     print("Not an Automorphic Number")

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -1,12 +1,12 @@
 n=int(input())
-c=0
-p=n
-while(n>0):
-    c+=1
-    n=n//10
-s=p**2
-r=s%(10**c)
-if(r==p):
+temp=n
+s=0
+while(n!=0):
+    n//=10
+    s=s+1
+q=temp**2
+p=q%(10**s)
+if(p==temp):
     print("Automorphic Number")
 else:
     print("Not an Automorphic Number")
